ffbeae9c3e6928e956a301acee5ffef55edd6118
hank <hearts.zhang@gmail.com>
2019-10-23 20:35:50 +0800

    Update nr-rrc-reader.c

--- nr-rrc-reader.c
+++ nr-rrc-reader.c
@@ -4,19 +4,19 @@ int PERDecodeRRCReconfiguration(unsigned short MsgID, unsigned char* pRawData,in
 
 int main(int argc, char* argv[])
 {
-	unsigned char buffer[410];
+    unsigned char buffer[410];
     const char* fn = "dl-dcch.dat";
     if (argc>1)
         fn = argv[1];
 
-	FILE *ptr = fopen(fn,"rb");  // r for read, b for binary
+    FILE *ptr = fopen(fn,"rb");  // r for read, b for binary
     if (!ptr) return -1;
 
-	fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
+    fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
 
     PERDecodeRRCReconfiguration(16640, buffer, sizeof(buffer) );
 
 
     fclose(ptr);
-	return 0;
+    return 0;
 }
